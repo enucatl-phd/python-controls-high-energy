7ce63db418202adc9ec5dc084ce0ff125dadafdd
Matteo Abis <gmatteo..abis@gmail.com>
2016-05-04 10:12:45 +0200

    create folder structure and install instructions

---
+++ README.md
@@ -0,0 +1,21 @@
+# Installation
+
+## Install pyenv
+```
+curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+PYTHON_CONFIGURE_OPTS="--enable-unicode=ucs4" pyenv install 2.7.11
+```
+
+## Install albula
+
+Download and unpack the albula code from [dectris.com](https://www.dectris.com/albula.html)
+
+```bash
+./ALBULA-3.2.0-2-x86_64.sh --python=python --accept-license --prefix=$HOME/bin
+```
+
+## Install control module
+
+```bash
+python setup.py develop
+```
---
+++ setup.py
@@ -0,0 +1,41 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='controls',
+    description='control the bunker 4 setup through epics',
+    long_description=long_description,
+    url="https://git.psi.ch/tomcat/python-controls-high-energy",
+    author='Matteo Abis',
+    author_email='matteo.abis@psi.ch',
+    license="MIT",
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+    ],
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    install_requires=[
+        'click',
+        'pyepics',
+        'pyserial',
+    ],
+    entry_points="""
+    [console_scripts]
+    bunker4controls = bin.bunker4controls:main
+    """
+)
