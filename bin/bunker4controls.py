af1c4daf1080d8e255495fa19244668784cd6fc7
Matteo Abis <gmatteo..abis@gmail.com>
2016-05-12 10:01:49 +0200

    prepare startup script

--- bin/bunker4controls.py
+++ bin/bunker4controls.py
@@ -1,7 +1,4 @@
 ########################################################################
-# 
-# gantry_control_init.py
-#
 # Initialize all needed packages and modules
 #
 # Author: Maria Buechner
@@ -12,70 +9,16 @@
 ########################################################################
 
 # Imports
-import threading
-import time
+import click
 import IPython
 
-# Imports need to be done like this, to only import the module once,
-# even if it is imported again in another module!
-
-# Camera control
-from shadobox_library import *
-
-# Tube control
-from tube_library import *
-
-# Gantry Motors
-# from gantry_motors import *
-
-# Gantry DPC functions
-from gantry_DPC_functions import *
+import controls.motors
 
 
-#########################################################################
-# Functions
-#########################################################################
+def main():
+    g0trx = controls.motors.Motor("", "")
+    IPython.embed()
 
-def quit():
-    """ Shutsdown all open devices gracefully
-        
-        Input parameters:
-            
-            none
-            
-        return parameters:
-            
-            none
-            
-    """
-    # Close camera (and disconnect from sever
-    if sb.is_camera_on():
-        sb.stop()
-    elif sb.client_socket: # if camera off, but still connected to server
-        sb.disconnect()
-    # Shutdown serial connection, if tube is connected
-    if tube.is_connected():
-        tube.stop()
-        
-    # TODO: motors?
-    
-    # Reminder message (display for 3 secs)
-    print('\n\n\n'
-    '-----------------------------------------------------------------------'
-    '\n\n'
-    '                        DETECTOR/TUBE TURNED OFF?'
-    '\n\n'
-    '-----------------------------------------------------------------------')
-    time.sleep(3)
-    
-    # Exit interactive interpreter
-    exit()
-    
 
-#########################################################################
-# Main
-#########################################################################
-    
-# Display introduction?
-
-IPython.embed()
+if __name__ == "__main__":
+    main()
