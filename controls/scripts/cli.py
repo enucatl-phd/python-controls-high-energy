7bf1c1eb7b4aa6a8410db18faf496c1ff03db1a7
Matteo Abis <gmatteo..abis@gmail.com>
2016-05-18 11:13:51 +0200

    new DEigerDetector class from dectris for controlling eiger

--- controls/scripts/cli.py
+++ controls/scripts/cli.py
@@ -14,11 +14,13 @@ import IPython
 import logging.config
 
 import controls.motors
+import controls.eiger
 import controls.log_config
 
 @click.command()
-@click.option("--debug", default=False)
+@click.option("--debug", is_flag=True)
 def main(debug):
     logging.config.dictConfig(controls.log_config.get_dict(debug))
     g0trx = controls.motors.Motor("X02DA-BNK-HE:G0_TRX", "g0trx")
+    eiger = controls.eiger.Eiger("129.129.99.99")
     IPython.embed()
