22426630f00019bc2be6b9449c0387e9afa3f4a0
Matteo Abis <gmatteo..abis@gmail.com>
2016-05-23 10:58:15 +0200

    fix digits in tube set kv and ma commands

--- controls/comet_tube.py
+++ controls/comet_tube.py
@@ -1,4 +1,8 @@
+import logging
 import serial
+
+logger = logging.getLogger(__name__)
+
 
 def send_string(serial, string):
     message = "{0}{1}{2}".format(
@@ -6,6 +10,7 @@ def send_string(serial, string):
         string,
         chr(13)
     )
+    logger.debug("sending %s to serial", message)
     serial.write(message.encode())
     return " ".join(serial.readline().decode().split()[1:])
 
@@ -32,7 +37,7 @@ class CometTube(object):
     @current.setter
     def current(self, value):
         return send_string(
-            self.serial, "I{0:.2f}".format(value)
+            self.serial, "I{0:.0f}".format(value * 100)
         )
 
     @property
@@ -42,7 +47,7 @@ class CometTube(object):
     @voltage.setter
     def voltage(self, value):
         return send_string(
-            self.serial, "U{0:.1f}".format(value)
+            self.serial, "U{0:.0f}".format(value * 10)
         )
 
     @property
@@ -59,4 +64,29 @@ class CometTube(object):
         return send_string(self.serial, "ON")
 
     def off(self):
-        return send_string(self.serial, "OF")      
+        return send_string(self.serial, "OF")
+
+
+if __name__ == '__main__':
+    tube = CometTube()
+    logging.basicConfig(level=logging.DEBUG)
+    print("current error code", tube.error_code)
+    print("voltage", tube.voltage)
+    print("current", tube.current)
+    print("focus", tube.focus)
+    print("set voltage to 120")
+    tube.voltage = 120
+    print("voltage", tube.voltage)
+    print("set voltage to 160")
+    tube.voltage = 160
+    print("voltage", tube.voltage)
+    print("set current to 5")
+    tube.current = 5
+    print("current", tube.current)
+    print("set current to 10")
+    tube.current = 10
+    print("current", tube.current)
+    tube.set_small_focus()
+    print(tube.focus)
+    tube.set_large_focus()
+    print(tube.focus)
