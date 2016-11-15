d42c52899890ab5975bb67213b81c3e6b4eb67fe
Matteo Abis <gmatteo..abis@gmail.com>
2016-11-15 11:43:25 +0100

    add log file

--- controls/lambda_detector.py
+++ controls/lambda_detector.py
@@ -8,13 +8,13 @@ import os
 logger = logging.getLogger(__name__)
 
 class Lambda(object):
-	"""
-	"""
-	def __init__(self,
-              host="hasmlmbd01",
-              port=10000,
-              photon_energy=10000,
-              storage_path="."):
+    """
+    """
+    def __init__(self,
+                 host="hasmlmbd01",
+                 port=10000,
+                 photon_energy=10000,
+                 storage_path="."):
 
             self.host = host
             self.storage_path = storage_path
@@ -28,65 +28,62 @@ class Lambda(object):
             self.device.EnergyThreshold = photon_energy
             self.device.SaveFilePath = "/localdata/psi/"
 
-
-        def trigger(self, exposure_time=1):
-            now = datetime.datetime.now()
-            self.device.FilePrefix = "snap_{0}".format(now.strftime("%H%M%S%f"))
-            self.device.FileStartNum = 0
-            self.device.ShutterTime = exposure_time * 1e3
-            self.device.command_inout("StartAcq") # start
-            time.sleep(exposure_time) # wait
-            logger.debug("writing file %s", self.device.FilePrefix)
-            cnt = 0
-            done = False
-            while not done and cnt < 100:
-                time.sleep(0.1)
-                cnt +=1
-                if self.device.state() == PyTango._PyTango.DevState.ON:
-                    done = True
-                    logger.info('... done.')
+    def trigger(self, exposure_time=1):
+        now = datetime.datetime.now()
+        self.device.FilePrefix = "snap_{0}".format(now.strftime("%H%M%S%f"))
+        self.device.FileStartNum = 0
+        self.device.ShutterTime = exposure_time * 1e3
+        self.device.command_inout("StartAcq") # start
+        time.sleep(exposure_time) # wait
+        logger.debug("writing file %s", self.device.FilePrefix)
+        cnt = 0
+        done = False
+        while not done and cnt < 100:
+            time.sleep(0.1)
+            cnt +=1
+            if self.device.state() == PyTango._PyTango.DevState.ON:
+                done = True
+                logger.info('... done.')
                 if cnt >= 100:
                     raise Exception("Detector timeout - acquisition failed")
-            return True
+        return True
 
+    def setNTrigger(self, intervals):
+        pass
 
-        def setNTrigger(self, intervals):
-            pass
+    def setFrameTime(self, exposure_time):
+        pass
 
-        def setFrameTime(self, exposure_time):
-            pass
+    def setCountTime(self, exposure_time):
+        pass
 
-        def setCountTime(self, exposure_time):
-            pass
+    def countTime(self):
+        pass
 
-        def countTime(self):
-            pass
+    def frameTime(self):
+        pass
 
-        def frameTime(self):
-            pass
+    def arm(self):
+        pass
 
-        def arm(self):
-            pass
+    def disarm(self):
+        pass
 
-        def disarm(self):
-            pass
+    def snap(self, exposure_time=1):
+        self.trigger(exposure_time)
+        self.save()
 
-	def snap(self, exposure_time=1):
-            self.trigger(exposure_time)
-            self.save()
-
-
-        def save(self):
-            now = datetime.datetime.now()
-            folder_name = os.path.join(
-                self.storage_path,
-                now.strftime("%H%M%S%f"))
-            subprocess.check_call(
-                "ssh e14980@x02da-gws-1 'mkdir {0}'".format(folder_name),
-                shell=True)
-            subprocess.check_call("scp {0}/*.nxs e14980@x02da-gws-1:{1}".format(
-                self.device.SaveFilePath,
-                folder_name), shell=True)
-            subprocess.check_call("rm {0}/*.nxs".format(
-                self.device.SaveFilePath), shell=True)
-            logger.info("images saved to %s", folder_name)
+    def save(self):
+        now = datetime.datetime.now()
+        folder_name = os.path.join(
+            self.storage_path,
+            now.strftime("%H%M%S%f"))
+        subprocess.check_call(
+            "ssh e14980@x02da-gws-1 'mkdir {0}'".format(folder_name),
+            shell=True)
+        subprocess.check_call("scp {0}/*.nxs e14980@x02da-gws-1:{1}".format(
+            self.device.SaveFilePath,
+            folder_name), shell=True)
+        subprocess.check_call("rm {0}/*.nxs".format(
+            self.device.SaveFilePath), shell=True)
+        logger.info("images saved to %s", folder_name)
--- controls/log_config.py
+++ controls/log_config.py
@@ -1,9 +1,16 @@
+import os
+
+
 def get_dict(verbose):
     levels = [
         'INFO',
         'DEBUG',
     ]
     verbose = min(len(levels) - 1, verbose)
+    now = datetime.datetime.now()
+    file_name = "log/bunker4controls.logfile.{0}.log.bz2".format(now.strftime("%y%m%d.%H%M%S%f"))
+    if not os.path.isdir("log"):
+        os.makedirs("log")
     return dict(
         version = 1,
         disable_existing_loggers = False,
@@ -17,10 +24,18 @@ def get_dict(verbose):
                 'class': 'logging.StreamHandler',
                 'formatter': 'f',
                 'level': levels[verbose],
+            },
+            'logfile': {
+                'class': 'logging.FileHandler',
+                'formatter': 'f',
+                'level': levels[verbose],
+                'filename': file_name,
+                'mode': 'w',
+                'encoding': 'bz2',
             }
         },
         root = {
-            'handlers': ['h'],
+            'handlers': ['h', 'logfile'],
             'level': levels[verbose],
         },
     )
