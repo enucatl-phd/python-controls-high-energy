3817d2225e126bce608b9690d7a03f6c7269735c
Matteo Abis <gmatteo..abis@gmail.com>
2018-03-21 11:38:53 +0100

    microfocus tube lab cdpc

--- controls/scripts/cli.py
+++ controls/scripts/cli.py
@@ -41,7 +41,7 @@ def main(verbose, storage_path, threshold):
     g0roty = controls.motors.Motor("X02DA-BNK-HE:G0_ROTY", "g0roty")
     g0rotz = controls.motors.Motor("X02DA-BNK-HE:G0_ROTZ", "g0rotz")
     g1trx = controls.motors.Motor("X02DA-BNK-HE:G1_TRX", "g1trx")
-    g1try = controls.motors.Motor("X02DA-BNK-HE:G1_TRY", "g1try")
+    disconnected = controls.motors.Motor("X02DA-BNK-HE:G1_TRY", "g1try")
     g1trz = controls.motors.Motor("X02DA-BNK-HE:G1_TRZ", "g1trz")
     g1rotx = controls.motors.Motor("X02DA-BNK-HE:G1_ROTX", "g1rotx")
     g1roty = controls.motors.Motor("X02DA-BNK-HE:G1_ROTY", "g1roty")
@@ -56,15 +56,20 @@ def main(verbose, storage_path, threshold):
     smpltry = controls.motors.Motor("X02DA-BNK-HE:SMPL_TRY", "smpltry")
     smplroty = controls.motors.Motor("X02DA-BNK-HE:SMPL_ROTY", "smplroty")
     stptrx = controls.motors.Motor("X02DA-BNK-HE:STP_TRX", "stptrx")
+    # compact dpc experiments
+    stptrx = controls.motors.Motor("X02DA-LAB-CDPC:SMARACT", "smaract")
+    g1rotz = controls.motors.Motor("X02DA-LAB-CDPC:SAM_TRY", "g1rotz")
+    g1trz = controls.motors.Motor("X02DA-LAB:TEST_M2", "g1trz")
+    smpltrx = controls.motors.Motor("X02DA-LAB-CDPC:SAM_TRX", "smpltrx")
     detector = controls.remote_detector.RemoteDetector(
-        "129.129.99.96",
+        "129.129.99.73",
         storage_path=storage_path,
         photon_energy=[threshold, 2*threshold]
     )
     # detector = controls.eiger.Eiger(
         # "129.129.99.112",
         # storage_path=storage_path,
-        # photon_energy=threshold
+    # photon_energy=threshold
     # )
     # detector = controls.pilatus.Pilatus(
      #   "129.129.99.81",
